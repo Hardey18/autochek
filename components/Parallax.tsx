9bb80a634096be57a59758b250434b1a93950bef
Nurudeen Adewale <nurudeenadeolaa@gmail.com>
2021-11-29 21:54:05 +0100

    modified the package.json file

--- components/Parallax.tsx
+++ components/Parallax.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import { Parallax, Background } from "react-parallax";
 import styles from '../styles/Parallax.module.css';
 
-import one from '../public/icons/one.jpeg'
-import two from '../public/icons/two.jpeg'
-import three from '../public/icons/three.jpeg'
-import four from '../public/icons/four.jpeg'
-import imageLoader from '../imageLoader';
-
 const Paralax = ({ children, bg }: any) => (
   <div className={styles.styles}>
     <Parallax strength={500}>
@@ -17,10 +11,7 @@ const Paralax = ({ children, bg }: any) => (
             height: 2000,
             width: 2000,
             backgroundImage: bg,
-            // backgroundPosition: "center",
-            // backgroundRepeat: "no-repeat",
             backgroundSize: "cover",
-            // position: "relative",
           }}
         />
       </Background>
